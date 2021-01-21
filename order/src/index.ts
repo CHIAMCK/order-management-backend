f59955cd55ba80c9bcf425689a33dbe771bb788b
CHIAM CHING KOK <chiamck91@hotmail.com>
2021-01-21 21:36:25 +0800

    setup db connection

--- order/src/index.ts
+++ order/src/index.ts
@@ -1,11 +1,28 @@
 import { json } from 'body-parser';
 import express from 'express';
 import routes from './routes';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(json());
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://order-mongo-srv:27017/order', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (err) {
+    // tslint:disable-next-line: no-console
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    // tslint:disable-next-line: no-console
+    console.log('Listening on port 3000');
+  });
+};
+
+start();
