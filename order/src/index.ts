b4fe3e43a7a9586a85ae1d42d7b7aaeddef1d39c
CHIAM CHING KOK <chiamck91@hotmail.com>
2021-01-21 20:09:19 +0800

    setup ingress nginx

--- order/src/index.ts
+++ order/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/v1/api/orders/current', (req, res) => {
+  res.send('Hi There');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
--- payment/src/index.ts
+++ payment/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/v1/api/payments/current', (req, res) => {
+  res.send('Hi There !!!');
+});
+
 app.listen(3001, () => {
   console.log('Listening on port 3001');
 });
